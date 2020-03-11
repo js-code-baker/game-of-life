938e0cf62e6755890092df85c845d554d9dc63fe
js-code-baker <sutezo@web.de>
2020-03-11 13:35:15 +0100

    Zwischenstand vom 11.03.2020

--- gol.js
+++ gol.js
@@ -4,19 +4,56 @@ window.onload = () => {
 
     // Startdaten
     let startdaten = setup();
-
+    let lebendeNachbarn = 0;
+    let toteNachbarn = 0;
     // Prüfungen (Aufgabe 1 - 4)
+    // Beispiel Aufgabe 1
+    if (startdaten [0][0] === 1) {
+      console.log('Meine erste Zelle lebt.');
+    } else {
+      console.log('Meine erste Zelle ist tot.');
+    }
 
+    if (startdaten [0][1] === 1) {
+      console.log('Und mein Rechter Nachbar, der lebt.')
+       lebendeNachbarn++;
+    } else {
+      console.log('Und mein Rechter Nachbar, der ist tot.')
+       toteNachbarn++;
+    }
+
+    if (startdaten [1][0] === 1) {
+      console.log('Die Zelle unter mir lebt.')
+       lebendeNachbarn++;
+    } else {
+      console.log('DIe Zelle unter mir ist tot.')
+       toteNachbarn++;
+    }
+
+    if (startdaten [1][1] === 1) {
+      console.log('Die Zelle schräk unter mir lebt.')
+       lebendeNachbarn++;
+    } else {
+      console.log('Die Zelle schräk unter mir ist tot.')
+       toteNachbarn++;
+    }
+      console.log('Meine erste Zelle hat '+ lebendeNachbarn +' lebendeNachbarn.');
+      console.log('Meine erste Zelle hat '+ toteNachbarn +' lebendeNachbarn.');
+
+    // startdaten -> hole Inhalt von position 0,0 check
+    // prüfe (if) Inhalt auf 0 oder 1
+    // bei Inhalt 0 "ist tot"
+    // bei Inhalt 1 "lebt"
     // Zeichnen (rendern) des Spielfeldes in der HTML Datei
-    for (let table = 0; table < 6; table++) {
-        renderTable(startdaten, table);
+    for (let tabelle = 0; tabelle < 6; tabelle++) {
+        renderTable(startdaten, tabelle);
     }
 
 }
 
 // Hier definieren wir die benötigten Funktionen
 function setup() {
-    let row =
+    let zeile =
         [
             [0, 1, 1, 0, 1, 0],
             [1, 0, 1, 0, 0, 1],
@@ -25,21 +62,21 @@ function setup() {
             [0, 1, 1, 0, 0, 1],
             [1, 1, 0, 1, 0, 0]
         ];
-    return row;
+    return zeile;
 }
 
-function renderTable(daten, rowCount) {
-    let table = holeHtmlTabelle();
-    let row = erzeugeTabellenZeile();
+function renderTable(daten, zeilenZahl) {
+    let tabelle = holeHtmlTabelle();
+    let zeile = erzeugeTabellenZeile();
 
-    for (let counter = 0; counter < 6; counter++) {
-        let cell = erzeugeTabellenZelle();
-        fügeDerZeileHinzu(row, cell);
-        if (daten[rowCount][counter] === 1) {
-            setzeZelleAufAlive(cell);
+    for (let zähler = 0; zähler < 6; zähler++) {
+        let zelle = erzeugeTabellenZelle();
+        fügeDerZeileHinzu(zeile, zelle);
+        if (daten[zeilenZahl][zähler] === 1) {
+            setzeZelleAufAlive(zelle);
         }
     }
-    fügeDerTabelleHinzu(table, row);
+    fügeDerTabelleHinzu(tabelle, zeile);
 }
 
 // Wrapper für JavaScript Funktionen
