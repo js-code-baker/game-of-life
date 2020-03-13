4afb06b4c3bb7fc81cffb4ab0ce7cda60b9991f0
js-code-baker <sutezo@web.de>
2020-03-13 16:19:11 +0100

    Letzter Stand vom 13.03.2020

--- gol.js
+++ gol.js
@@ -1,199 +1,257 @@
 
-window.onload = () => {
-    // Der Bereich mit der Action, die man im Browser sieht
+window.onload = function () {
+  // Der Bereich mit der Action, die man im Browser sieht
+  var knöpfchen = holeKnopfMitId('knopf');
+  knöpfchen.addEventListener('click', myButton);
+  // Startdaten
+  let startdaten = setup();
+  let ersteRunde = setup();
+  let lebendeNachbarn = 0;
+  let toteNachbarn = 0;
 
-    // Startdaten
-    let startdaten = setup();
-    let ersteRunde = setup();
-    let lebendeNachbarn = 0;
-    let toteNachbarn = 0;
+  if (startdaten[0][0] === 1) {
+    console.log('Meine erste Zelle lebt.');
+  } else {
+    console.log('Meine erste Zelle ist tot.');
+  }
 
-    if (startdaten [0][0] === 1) {
-      console.log('Meine erste Zelle lebt.');
-    } else {
-      console.log('Meine erste Zelle ist tot.');
+  if (startdaten[0][1] === 1) {
+    console.log('Und mein Rechter Nachbar, der lebt.')
+    lebendeNachbarn++;
+  } else {
+    console.log('Und mein Rechter Nachbar, der ist tot.')
+    toteNachbarn++;
+  }
+
+  if (startdaten[1][0] === 1) {
+    console.log('Die Zelle unter mir lebt.')
+    lebendeNachbarn++;
+  } else {
+    console.log('DIe Zelle unter mir ist tot.')
+    toteNachbarn++;
+  }
+
+  if (startdaten[1][1] === 1) {
+    console.log('Die Zelle schräk unter mir lebt.')
+    lebendeNachbarn++;
+  } else {
+    console.log('Die Zelle schräk unter mir ist tot.')
+    toteNachbarn++;
+  }
+  console.log('Meine erste Zelle hat ' + lebendeNachbarn + ' lebendeNachbarn.');
+  console.log('Meine erste Zelle hat ' + toteNachbarn + ' lebendeNachbarn.');
+
+  //Aufgabe 5:
+
+  if (startdaten[0][0] === 0 &&
+    startdaten[0][1] === 1 &&
+    startdaten[1][0] === 1 &&
+    startdaten[1][1] === 1) {
+    ersteRunde[0][0] = 1; console.log('Ich lebe in der nächten Runde.');
+  } else {
+    console.log('Ich bin weiterhin tot.');
+
+    //Aufgabe 6
+    let anzahlNachbarn = 0;
+
+    if (startdaten[0][1] === 1) { anzahlNachbarn++; }
+    if (startdaten[1][0] === 1) { anzahlNachbarn++; }
+    if (startdaten[1][1] === 1) { anzahlNachbarn++; }
+
+    if (startdaten[0][0] === 1 && anzahlNachbarn === 2 || anzahlNachbarn === 3) {
+      ersteRunde[0][0] = 1;
+      console.log('>>Ich bin weiterhin am leben')
+    };
+
+
+    /* if (startdaten[0][0] === 1 && startdaten[0][1] === 1 && startdaten[1][0] === 1 ||
+                                  startdaten[0][1] === 1 && startdaten[1][1] === 1 ||
+                                  startdaten[1][0] === 1 && startdaten[1][1] === 1 ||
+                                  startdaten[0][1] === 1 && startdaten[1][0] === 1 &&
+                                  startdaten[1][1] === 1) {
+                          startdaten[0][0] = 1; console.log('Ich bin weiterhin am leben.')
+                        }*/
+    // Aufgabe 7
+    if (startdaten[0][0] === 1 && startdaten[0][1] === 0 && startdaten[1][0] === 0 &&
+      startdaten[1][1] === 1 ||
+      startdaten[0][1] === 0 && startdaten[1][1] === 0 &&
+      startdaten[1][0] === 1 ||
+      startdaten[1][1] === 0 && startdaten[1][0] === 0 &&
+      startdaten[0][1] === 1) {
+      ersteRunde[0][0] = 0;
+      console.log('Ich werde sterben.')
     }
+    //Aufgabe 8
+    let anzahlNachbarn1 = 0;
 
-    if (startdaten [0][1] === 1) {
-      console.log('Und mein Rechter Nachbar, der lebt.')
-       lebendeNachbarn++;
-    } else {
-      console.log('Und mein Rechter Nachbar, der ist tot.')
-       toteNachbarn++;
-    }
+    if (startdaten[0][1] === 1) { anzahlNachbarn1++; }
+    if (startdaten[1][0] === 1) { anzahlNachbarn1++; }
+    if (startdaten[1][1] === 1) { anzahlNachbarn1++; }
 
-    if (startdaten [1][0] === 1) {
-      console.log('Die Zelle unter mir lebt.')
-       lebendeNachbarn++;
-    } else {
-      console.log('DIe Zelle unter mir ist tot.')
-       toteNachbarn++;
-    }
+    if (startdaten[0][0] === 1 && anzahlNachbarn1 > 3) {
+      ersteRunde[0][0] = 0;
+      console.log('Ich werde sterben !')
+    };
 
-    if (startdaten [1][1] === 1) {
-      console.log('Die Zelle schräk unter mir lebt.')
-       lebendeNachbarn++;
-    } else {
-      console.log('Die Zelle schräk unter mir ist tot.')
-       toteNachbarn++;
-    }
-      console.log('Meine erste Zelle hat '+ lebendeNachbarn +' lebendeNachbarn.');
-      console.log('Meine erste Zelle hat '+ toteNachbarn +' lebendeNachbarn.');
+    //Aufgabe 13 1.Zelle
 
-//Aufgabe 5:
+    // AUfgabe 13 2.Zelle
 
-if  ( startdaten[0][0] === 0 &&
-      startdaten[0][1] === 1 &&
-      startdaten[1][0] === 1 &&
-      startdaten[1][1] === 1)  {
-      ersteRunde[0][0] = 1;  console.log('Ich lebe in der nächten Runde.');
-} else {
-   console.log('Ich bin weiterhin tot.');
+    //Aufgabe 13 3.Zelle
 
-//Aufgabe 6
-let anzahlNachbarn = 0;
+    //Aufgabe 13 4.Zelle
 
-if (startdaten[0][1] === 1) { anzahlNachbarn++; }
-if (startdaten[1][0] === 1) { anzahlNachbarn++; }
-if (startdaten[1][1] === 1) { anzahlNachbarn++; }
+    //Aufgab 13 5.Zelle
 
-if (startdaten[0][0] === 1 && anzahlNachbarn === 2 || anzahlNachbarn === 3) {
-   ersteRunde[0][0] = 1;
-   console.log('>>Ich bin weiterhin am leben')
- };
+    //Aufgabe 12 6.Zelle
 
-
-/* if (startdaten[0][0] === 1 && startdaten[0][1] === 1 && startdaten[1][0] === 1 ||
-                              startdaten[0][1] === 1 && startdaten[1][1] === 1 ||
-                              startdaten[1][0] === 1 && startdaten[1][1] === 1 ||
-                              startdaten[0][1] === 1 && startdaten[1][0] === 1 &&
-                              startdaten[1][1] === 1) {
-                      startdaten[0][0] = 1; console.log('Ich bin weiterhin am leben.')
-                    }*/
-// Aufgabe 7
-if (startdaten[0][0] === 1 && startdaten[0][1] === 0 && startdaten [1][0] === 0 &&
-                               startdaten[1][1] === 1 ||
-                               startdaten[0][1] === 0 && startdaten [1][1] === 0 &&
-                               startdaten[1][0] === 1 ||
-                               startdaten[1][1] === 0 && startdaten [1][0] === 0 &&
-                               startdaten[0][1] === 1 ) {
-                                 ersteRunde[0][0] = 0;
-                                 console.log('Ich werde sterben.')
-                               }
-//Aufgabe 8
-let anzahlNachbarn1 = 0;
-
-if (startdaten[0][1] === 1) { anzahlNachbarn1++; }
-if (startdaten[1][0] === 1) { anzahlNachbarn1++; }
-if (startdaten[1][1] === 1) { anzahlNachbarn1++; }
-
-if (startdaten[0][0] === 1 && anzahlNachbarn1 > 3) {
-   ersteRunde[0][0] = 0;
-   console.log('Ich werde sterben !')
- };
-
-//Aufgabe 13 1.Zelle
-  berechneNachbarn(startdaten,0,0);
-// AUfgabe 13 2.Zelle
-  berechneNachbarn(startdaten,0,1);
-//Aufgabe 13 3.Zelle
-  berechneNachbarn(startdaten,0,2);
-//Aufgabe 13 4.Zelle
-  berechneNachbarn(startdaten,0,3);
-//Aufgab 13 5.Zelle
-  berechneNachbarn(startdaten,0,4);
-  //Aufgabe 12 6.Zelle
-  berechneNachbarn(startdaten,0,5);
     // Zeichnen (rendern) des Spielfeldes in der HTML Datei
     for (let tabelle = 0; tabelle < 6; tabelle++) {
-        renderTable(startdaten, tabelle);
+      renderTable(startdaten, tabelle);
     }
 
+  }
+
+  // Hier definieren wir die benötigten Funktionen
+  function berechneNachbarn(daten, y, x) {
+    let lebendeNachbarn3 = 0;
+    let toteNachbarn3 = 0;
+
+    try {
+      if (daten[y - 1][x - 1] === 1) { lebendeNachbarn3++ }
+      else if (daten[y - 1][x - 1] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      debugger;
+      if (daten[y][x - 1] === 1) { lebendeNachbarn3++ }
+      else if (daten[y][x - 1] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      if (daten[y + 1][x - 1] === 1) { lebendeNachbarn3++ }
+      else if (daten[y + 1][x - 1] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      if (daten[y + 1][x] === 1) { lebendeNachbarn3++ }
+      else if (daten[y + 1][x] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      if (daten[y + 1][x + 1] === 1) { lebendeNachbarn3++ }
+      else if (daten[y + 1][x + 1] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      if (daten[y][x + 1] === 1) { lebendeNachbarn3++ }
+      else if (daten[y][x + 1] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      if (daten[y - 1][x + 1] === 1) { lebendeNachbarn3++ }
+      else if (daten[y - 1][x + 1] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+    try {
+      if (daten[y - 1][x] === 1) { lebendeNachbarn3++ }
+      else if (daten[y - 1][x] === 0) { toteNachbarn3++ }
+    } catch (Exception) {
+     // console.log('Nachbar nicht vorhanden');
+    }
+
+
+    if (daten[y][x] === 1) {
+      console.log('Ich bin die ' + (6 * y + x + 1) + '.Zelle,ich lebe und ich habe ' +
+        lebendeNachbarn3 + ' lebende Nachbarn, und ' + toteNachbarn3 + ' tote Nachbarn')
+    } else {
+      console.log('Ich bin die ' + (6 * y + x + 1) + '.Zelle,ich bin tot und ich habe ' +
+        lebendeNachbarn3 + ' lebende Nachbarn, und ' + toteNachbarn3 + ' tote Nachbarn');
+    }
+
+  
+
+  //aufgabe 14
+
+  if (daten[y][x] === 0 && lebendeNachbarn3 === 3) { ersteRunde[y][x] = 1 }
+  if (daten[y][x] === 1 && lebendeNachbarn3 < 2) { ersteRunde[y][x] = 0 }
+  if (daten[y][x] === 1 && (lebendeNachbarn3 === 2
+    || lebendeNachbarn3 === 3)) { ersteRunde[y][x] = 1 }
+  if (daten[y][x] === 1 && lebendeNachbarn3 > 3) { ersteRunde[y][x] = 0 }
+
+  console.log(ersteRunde[y][x])
 }
-
-// Hier definieren wir die benötigten Funktionen
-function berechneNachbarn(daten, y, x) {
-  let lebendeNachbarn3 = 0;
-  let toteNachbarn3 = 0;
-
-  if (daten[y][x-1] === 1) { lebendeNachbarn3++}
-     else if (daten[y][x-1] === 0){toteNachbarn3++}
-  if (daten[y+1][x-1] === 1) { lebendeNachbarn3++}
-     else if (daten[y+1][x-1] === 0){toteNachbarn3++}
-  if (daten[y+1][x] === 1) { lebendeNachbarn3++}
-     else if (daten[y+1][x] === 0){toteNachbarn3++}
-  if (daten[y+1][x+1] === 1) { lebendeNachbarn3++}
-     else if (daten[y+1][x+1] === 0){toteNachbarn3++}
-  if (daten[y][x+1] === 1) { lebendeNachbarn3++}
-     else if (daten[y][x+1] === 0){toteNachbarn3++}
-
-  if (daten[y][x] === 1) { console.log('Ich bin die '+ (6*y + x + 1) +'.Zelle,ich lebe und ich habe '+
-  lebendeNachbarn3 +' lebende Nachbarn, und '+toteNachbarn3 +' tote Nachbarn')} else {
-    console.log('Ich bin die '+ (6*y + x + 1) +'.Zelle,ich bin tot und ich habe '+
-    lebendeNachbarn3+ ' lebende Nachbarn, und '+toteNachbarn3+ ' tote Nachbarn');}
-
-
-//aufgabe 14
-
- if (daten[y][x] === 0 && lebendeNachbarn3 === 3){ersteRunde[y][x] = 1}
- if (daten[y][x] === 1 && lebendeNachbarn3 < 2){ersteRunde[y][x] = 0}
- if (daten[y][x] === 1 && lebendeNachbarn3 === 2
-                       || lebendeNachbarn3 === 3){ersteRunde[y][x] = 1}
- if (daten[y][x] === 1 && lebendeNachbarn3 > 3){ersteRunde[y][x] = 0}
-
-console.log(ersteRunde[y][x])}
 
 
 function setup() {
-    let zeile =
-        [
-            [1, 1, 1, 0, 1, 0],
-            [1, 0, 1, 0, 0, 1],
-            [0, 0, 1, 1, 0, 0],
-            [1, 1, 0, 0, 1, 1],
-            [0, 1, 1, 0, 0, 1],
-            [1, 1, 0, 1, 0, 0]
-        ];
-    return zeile;
+  let zeile =
+    [
+      [1, 1, 1, 0, 1, 0],
+      [1, 0, 1, 0, 0, 1],
+      [0, 0, 1, 1, 0, 0],
+      [1, 1, 0, 0, 1, 1],
+      [0, 1, 1, 0, 0, 1],
+      [1, 1, 0, 1, 0, 0]
+    ];
+  return zeile;
 }
 
 function renderTable(daten, zeilenZahl) {
-    let tabelle = holeHtmlTabelle();
-    let zeile = erzeugeTabellenZeile();
+  let tabelle = holeHtmlTabelle();
+  let zeile = erzeugeTabellenZeile();
 
-    for (let zähler = 0; zähler < 6; zähler++) {
-        let zelle = erzeugeTabellenZelle();
-        fügeDerZeileHinzu(zeile, zelle);
-        if (daten[zeilenZahl][zähler] === 1) {
-            setzeZelleAufAlive(zelle);
-        }
+  for (let zähler = 0; zähler < 6; zähler++) {
+    let zelle = erzeugeTabellenZelle();
+    fügeDerZeileHinzu(zeile, zelle);
+    if (daten[zeilenZahl][zähler] === 1) {
+      setzeZelleAufAlive(zelle);
     }
-    fügeDerTabelleHinzu(tabelle, zeile);
+  }
+  fügeDerTabelleHinzu(tabelle, zeile);
 }
 
 // Wrapper für JavaScript Funktionen
 function setzeZelleAufAlive(zelle) {
-    zelle.className += " alive";
+  zelle.className += " alive";
 }
 
 function fügeDerTabelleHinzu(tabelle, zeile) {
-    tabelle.appendChild(zeile);
+  tabelle.appendChild(zeile);
 }
 
 function fügeDerZeileHinzu(zeile, zelle) {
-    zeile.appendChild(zelle);
+  zeile.appendChild(zelle);
 }
 
 function erzeugeTabellenZelle() {
-    return document.createElement('td');
+  return document.createElement('td');
 }
 
 function erzeugeTabellenZeile() {
-    return document.createElement('tr');
+  return document.createElement('tr');
 }
 
 function holeHtmlTabelle() {
-    return document.getElementById('gol-container');
+  return document.getElementById('gol-container');
+}
+
+function holeKnopfMitId(id) {
+  return document.getElementById(id);
+}
+
+function myButton() {
+  console.log();
+  for (let zeilenÜbergang = 0; zeilenÜbergang < 6; zeilenÜbergang++) {
+    for (let nächsteRundeZelle = 0; nächsteRundeZelle < 6; nächsteRundeZelle++) {
+      berechneNachbarn(startdaten, zeilenÜbergang, nächsteRundeZelle);
+    }
+  }
 }
 }
-function myButton() {console.log('Da hat jemand den Knopf gedrückt!')}
